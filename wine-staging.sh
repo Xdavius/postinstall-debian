f492b674625c885eccba7661c5c611703a7941b4
Davius <66792063+Xdavius@users.noreply.github.com>
2023-06-14 15:52:21 +0200

    Update wine-staging.sh
    Add root checker

--- wine-staging.sh
+++ wine-staging.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
-sudo apt update
-sudo apt install --install-recommends winehq-staging
+
+# Root checker
+if [[ $EUID -ne 0 ]]; then
+	echo -e "\n---------------------------------------\n"
+    echo -e "You need to be logged as root (su- / sudo -i)" 1>&2
+    echo -e "\n---------------------------------------\n"
+	exit 1
+
+echo "Job start : Installing deb-get"
+sleep 2
+
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+apt update
+apt install -y --install-recommends winehq-staging
+
+echo "Job done"
