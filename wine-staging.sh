25dbdbdec321812656a567c623b2526324498ce4
Davius <66792063+Xdavius@users.noreply.github.com>
2023-06-14 15:43:27 +0200

    Mesa + Wine
    For Debian 12

---
+++ wine-staging.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+sudo apt update
+sudo apt install --install-recommends winehq-staging
