50013b3491b2d21302f48c5c18acf1f18a3d98ff
Davius <66792063+Xdavius@users.noreply.github.com>
2023-06-29 13:50:10 +0200

    Update nvidia-testing.sh

--- nvidia-testing.sh
+++ nvidia-testing.sh
@@ -32,9 +32,9 @@ if [[ $select == "" ]];
 	apt install -y wget
 	echo "deb http://deb.debian.org/debian experimental non-free-firmware contrib non-free main" > /etc/apt/sources.list.d/experimental.list
 	apt update
-	apt install -y -t experimental nvidia-driver libvulkan* firmware-misc-nonfree nvidia-settings nvidia-cuda-mps vulkan-utils
-	apt install -y -t experimental libvulkan*:i386 nvidia-driver-libs:i386 vulkan-utils:i386
-	apt install -y -t experimental nvidia-cuda-toolkit nvidia-cuda-dev
+	apt install -y -t experimental nvidia-driver vulkan-tools libvulkan* firmware-misc-nonfree nvidia-settings
+	apt install -y -t experimental libvulkan*:i386 nvidia-driver-libs:i386
+	apt install -y -t experimental nvidia-cuda-toolkit nvidia-cuda-dev nvidia-cuda-mps
 	apt autoremove -y
 	echo "Job done"
 	echo "Veuillez REBOOT la machine !!"
