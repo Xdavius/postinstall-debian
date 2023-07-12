dbd69bb33f5b0195d54d8990edcd4412ca5328eb
Davius <66792063+Xdavius@users.noreply.github.com>
2023-07-12 13:52:17 +0200

    Add files via upload

--- data/install-sid.sh
+++ data/install-sid.sh
@@ -15,19 +15,25 @@ sleep 2
 
 if [ ! -x /etc/apt/sources.list.d/sid.list ]
 then
-    cp ./sid.list /etc/apt/sources.list.d/sid.list
+	echo "deb http://deb.debian.org/debian/ sid main contrib non-free-firmware non-free" >> /etc/apt/sources.list.d/sid.list
+else
+	echo "Le fichier /etc/apt/sources.list.d/sid.list existe déjà !!"
 fi
-
-echo "Le fichier /etc/apt/sources.list.d/sid.list existe déjà !!"
 
 if [ ! -x /etc/apt/preferences.d/sid ]
 then
-    cp ./sid /etc/apt/preferences.d/sid
+	echo "Package : *" > /etc/apt/sources.list.d/sid.list
+	echo "Pin : release a=unstable" >> /etc/apt/sources.list.d/sid.list
+	echo "Pin-Priority : 10" >> /etc/apt/sources.list.d/sid.list
+else
+	echo "Le fichier /etc/apt/preferences.d/sid existe déjà !!"
 fi
 
-echo "Le fichier /etc/apt/preferences.d/sid existe déjà !!"
-
 apt update
+
+echo "
+Job done.
+"
 
 echo "
 Job done.
