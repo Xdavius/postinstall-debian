2bfe0abd65b70b8fe79411593e79d7311e8793fc
Davius <66792063+Xdavius@users.noreply.github.com>
2023-07-12 10:54:40 +0200

    Add files via upload
    ajout de postinstall-debian-tui

---
+++ source/scripts.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+function nvidia-stable() {
+bash ./data/nvidia-stable.sh
+sleep 2
+nvidia1
+}
+
+function nvidia-testing() {
+bash ./data/nvidia-testing.sh
+sleep 2
+nvidia1
+}
+
+function nvidia-rollback() {
+bash ./data/nvidia-rollback.sh
+sleep 2
+nvidia1
+}
+
+function secureboot() {
+bash ./data/secureboot.sh
+sleep 2
+nvidia1
+}
+
+function mesa-kisak() {
+bash ./data/mesa-kisak-fresh.sh
+sleep 2
+amd1
+}
+
+function amd-vulkan() {
+clear
+bash ./data/amd-vulkan.sh
+sleep 2
+amd1
+}
+
+function deb-get() {
+clear
+bash ./data/deb-get.sh
+sleep 2
+tools1
+}
+
+function wine-staging() {
+clear
+bash ./data/wine-staging.sh
+sleep 2
+tools1
+}
+
+function add-ppa-debian() {
+clear
+echo "------------------------------------------------------
+"
+read -p 'Saisir le nom du ppa au format "ppa:nom/repository" : ' $1
+bash ./data/add-ppa-debian.sh $1
+sleep 2
+tools1
+}
+
+function install-sid() {
+clear
+bash ./data/install-sid.sh
+sleep 2
+tools1
+}
