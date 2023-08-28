be1efe3ba24dc419be7f6b583d652c709be80762
Davius <66792063+Xdavius@users.noreply.github.com>
2023-08-28 15:03:31 +0200

    Update mesa-kisak-fresh.sh

--- data/mesa-kisak-fresh.sh
+++ data/mesa-kisak-fresh.sh
@@ -8,17 +8,17 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-echo "Job start : Installing mesa-kisak-fresh"
+clear
+echo "Job start : Installing mesa-kisak-fresh" > /var/log/$LOGNAME.auto-update.txt 2>&1
 sleep 2
 
 echo "deb https://ppa.launchpadcontent.net/kisak/kisak-mesa/ubuntu jammy main" > /etc/apt/sources.list.d/mesa-kisak-fresh.list && \
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F63F0F2B90935439 && \
 mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/mesa-kisak-fresh.gpg &&\
-apt-get update &&\
-apt-get full-upgrade -y
+apt-get update >> /var/log/$LOGNAME.auto-update.txt 2>&1 &&\
+apt-get full-upgrade -y >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
 echo "
 Job done.
 "
 
-sleep 10
