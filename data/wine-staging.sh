94cf73010d80622a2056d6f08433caff05b68f31
Davius <66792063+Xdavius@users.noreply.github.com>
2023-08-28 15:10:01 +0200

    Update wine-staging.sh

--- data/wine-staging.sh
+++ data/wine-staging.sh
@@ -8,14 +8,17 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-echo "Job start : Installing wine-staging"
-sleep 2
+echo "Job start : Installing wine-staging
+
+Patientez...
+" 
+sleep 2 > /var/log/$LOGNAME.auto-update.txt 2>&1
 
 dpkg --add-architecture i386
 wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
-apt update
-apt install -y python3-gi-cairo
-apt install -y --install-recommends winehq-staging
+apt update >> /var/log/$LOGNAME.auto-update.txt 2>&1
+apt install -y python3-gi-cairo >> /var/log/$LOGNAME.auto-update.txt 2>&1
+apt install -y --install-recommends winehq-staging >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
 echo "Job done"
