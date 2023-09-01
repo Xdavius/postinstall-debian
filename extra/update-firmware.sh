2ce899be629fcf8b3f886a4ba97d518fecf7bbda
Davius <66792063+Xdavius@users.noreply.github.com>
2023-09-01 21:52:37 +0200

    Update update-firmware.sh

--- extra/update-firmware.sh
+++ extra/update-firmware.sh
@@ -19,8 +19,11 @@ Job start : Updating linux-firmware to latest git
 "
 sleep 2
 
+echo "Téléchargement en cours..."
 wget -c https://gitlab.com/kernel-firmware/linux-firmware/-/archive/main/linux-firmware-main.tar.gz -O - | tar -xz
+echo "Installation..."
 mv linux-firmware-main/* /usr/lib/firmware
+echo "Nettoyage..."
 rm -rf linux-firmware-main
 
 echo "
