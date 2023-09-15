cfc53ef1695faf58de527b824cdf28a4105b8b5c
Davius <66792063+Xdavius@users.noreply.github.com>
2023-09-15 15:14:41 +0200

    Update INSTALL.sh

--- SECUREBOOT/INSTALL.sh
+++ SECUREBOOT/INSTALL.sh
@@ -7,8 +7,13 @@ if [[ $EUID -ne 0 ]]; then
     echo -e "\n---------------------------------------\n"
 	exit 1
 fi
+apt install -f dkms
+
+kernel_ver=(uname -r)
 srcdir=$(pwd)
 chmod +x data/*.sh
 chmod +x data/src/zz-signing
 source $srcdir/data/secureboot.sh
 source $srcdir/data/install-kernel-autosign.sh
+mkdir -p /opt/signtool/
+cp /lib/modules/$kernel_ver/build/scripts/sign-file /opt/signtool
