7ff6fb0d46e16ea3323e52dd0b32417925d33ca3
Davius <66792063+Xdavius@users.noreply.github.com>
2023-09-16 19:30:17 +0200

    Create lutris-latest.sh

---
+++ data/lutris-latest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Root checker
+if [[ $EUID -ne 0 ]]; then
+	echo -e "\n---------------------------------------\n"
+    echo -e "You need to be logged as root (su- / sudo -i)" 1>&2
+    echo -e "\n---------------------------------------\n"
+	exit 1
+fi
+
+echo "Job start : Installing Lutris Latest
+
+Patientez...
+" 
+sleep 2 > /var/log/$LOGNAME.auto-update.txt 2>&1
+
+echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
+apt update
+apt install lutris >> /var/log/$LOGNAME.auto-update.txt 2>&1
+
+echo "Job done"
+
+sleep 2
