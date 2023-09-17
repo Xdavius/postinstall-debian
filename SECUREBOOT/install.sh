5afd0b9b47bb544131e713294c25998f658fde84
Davius <66792063+Xdavius@users.noreply.github.com>
2023-09-17 14:01:53 +0200

    Update install.sh

--- SECUREBOOT/install.sh
+++ SECUREBOOT/install.sh
@@ -8,10 +8,11 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 clear
-echo "Configuration du système SECUREBOOT
+echo "
+Configuration du système SECUREBOOT
 
 Installation des dépendances...
-"
+" ; sleep 1
 apt install -f dkms patch > /var/log/$LOGNAME.auto-update.txt 2>&1
 
 kernel_ver=$(uname -r)
@@ -19,14 +20,24 @@ srcdir=$(pwd)
 chmod +x data/*.sh
 chmod +x data/src/zz-signing
 
-echo "installation de sign-file pour les Kernels Customs"
+echo "
+installation de sign-file pour les Kernels Customs...
+" ; sleep 1
 mkdir -p /opt/signtool/
 cp /lib/modules/$kernel_ver/build/scripts/sign-file /opt/signtool
 
-echo "Patch de dkms pour les kernels Customs, blocage des mises à jour du paquet"
+echo "
+Patch de dkms pour les kernels Customs, blocage des mises à jour du paquet
+" ; sleep 1
 patch -i $srcdir/data/dkms.patch /usr/sbin/dkms
 apt-mark hold dkms
 
-echo "Installation..."
+echo "
+Installation...
+" ; sleep 1
 source $srcdir/data/secureboot.sh
 source $srcdir/data/install-kernel-autosign.sh
+
+echo "
+Job done
+" ; sleep 2
