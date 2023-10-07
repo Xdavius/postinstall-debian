e6d4a4596ffaa9cd4f5b123fedae3b319b45c184
Davius <66792063+Xdavius@users.noreply.github.com>
2023-10-07 12:39:00 +0200

    Update lutris-latest.sh

--- data/lutris-latest.sh
+++ data/lutris-latest.sh
@@ -9,20 +9,20 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Job start : Installing Lutris Latest
-
-Patientez...
-" 
-sleep 2 > /var/log/$LOGNAME.auto-update.txt 2>&1
+" ; sleep 2
 
 echo "Ajout du dépôt lutris.list"
 echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+
 echo "Installation de la clé GPG"
 wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
+
 echo "Rafraichissement des dépôts"
-apt update >> /var/log/$LOGNAME.auto-update.txt 2>&1
+apt update > /var/log/$LOGNAME.auto-update.txt 2>&1
+
 echo "Installation de Lutris"
 apt install -y lutris >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
-echo "Job done"
-
-sleep 2
+echo "
+Job done
+"; sleep 2
