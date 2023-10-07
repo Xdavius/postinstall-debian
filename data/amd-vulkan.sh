ec7562836a53cd2c8218d62e59ecda4fdfb487d1
Davius <66792063+Xdavius@users.noreply.github.com>
2023-10-07 12:28:11 +0200

    Update amd-vulkan.sh

--- data/amd-vulkan.sh
+++ data/amd-vulkan.sh
@@ -11,17 +11,16 @@ fi
 clear
 echo "
 Installation de Vulkan AMD/Intel
-"
-sleep 2
+"; sleep 2
 
+echo "Vérification des dépôts additionnels"; sleep 1
 dpkg --add-architecture i386
 apt-add-repository -y contrib > /var/log/$LOGNAME.auto-update.txt 2>&1
 apt-add-repository -y non-free >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
+echo "Installation..."
 apt install -y libgl1-mesa-dri libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 vulkan-tools >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
 echo "
 Job done
-"
-
-sleep 2
+"; sleep 2
