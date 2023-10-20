1db5a94dc43c8c41570994ea3e62e43a1f35bd09
Davius <66792063+Xdavius@users.noreply.github.com>
2023-10-20 16:18:14 +0200

    Update nvidia-rollback.sh

--- data/nvidia-rollback.sh
+++ data/nvidia-rollback.sh
@@ -13,7 +13,7 @@ echo "Job start : Uninstalling Nvidia Drivers
 "; sleep 2
 
 apt autopurge -y cuda-keyring nvidia-cuda-toolkit nvidia-cuda-dev nvidia-cuda-mps > /var/log/$LOGNAME.auto-update.txt 2>&1
-apt autopurge -y nvidia-driver vulkan-tools firmware-misc-nonfree nvidia-settings libglvnd-dev libvulkan*:i386 nvidia-driver-libs:i386 nvidia-cuda-toolkit nvidia-cuda-dev nvidia-cuda-mps >> /var/log/$LOGNAME.auto-update.txt 2>&1
+apt autopurge -y nvidia-driver cuda cuda-drivers vulkan-tools firmware-misc-nonfree nvidia-settings libglvnd-dev libvulkan*:i386 nvidia-driver-libs:i386 nvidia-cuda-toolkit nvidia-cuda-dev nvidia-cuda-mps >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
 echo "Veuillez REBOOT la machine !!"
 
