093b5866b404e22ff186faff68fb49a0783a3dfc
Davius <66792063+Xdavius@users.noreply.github.com>
2023-11-09 11:59:11 +0100

    Add files via upload

---
+++ extra/envycontrol/envycontrol.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/Xdavius/envycontrol/releases/download/3.3/python3-envycontrol_3.3.1-1_all.deb
+sudo apt install ./python3-envycontrol_3.3.1-1_all.deb
+rm python3-envycontrol_3.3.1-1_all.deb
---
+++ extra/envycontrol/install-optimus-gpu-switcher-kde.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone https://github.com/enielrodriguez/optimus-gpu-switcher.git
+plasmapkg2 -i optimus-gpu-switcher
