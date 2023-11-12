d9f14225448323a58e8df3ff9578bb000837deb7
Davius <66792063+Xdavius@users.noreply.github.com>
2023-11-12 13:28:58 +0100

    Delete extra/envycontrol/envycontrol.sh

--- extra/envycontrol/envycontrol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Xdavius/envycontrol/releases/download/3.3/python3-envycontrol_3.3.1-1_all.deb
-sudo apt install ./python3-envycontrol_3.3.1-1_all.deb
-rm python3-envycontrol_3.3.1-1_all.deb
