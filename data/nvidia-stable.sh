732aa0b846631130c715ed6722f6b04bec17b4ef
Davius <66792063+Xdavius@users.noreply.github.com>
2023-11-19 13:11:30 +0100

    Update nvidia-stable.sh

--- data/nvidia-stable.sh
+++ data/nvidia-stable.sh
@@ -34,7 +34,7 @@ dpkg --add-architecture i386 >> /var/log/$LOGNAME.auto-update.txt 2>&1
 add-apt-repository -y contrib >> /var/log/$LOGNAME.auto-update.txt 2>&1
 add-apt-repository -y non-free >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
-apt install -y linux-headers-amd64 build-essential dkms libglvnd-dev firmware-misc-nonfree pkg-config wget >> /var/log/$LOGNAME.auto-update.txt 2>&1
+apt install -y linux-headers-amd64 build-essential dkms libglvnd-dev pkg-config firmware-misc-nonfree pkg-config wget >> /var/log/$LOGNAME.auto-update.txt 2>&1
 
 echo "
 Nettoyage du système :
