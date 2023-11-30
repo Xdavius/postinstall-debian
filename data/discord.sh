0d6c81ff5c194411d031de9228423b1e0cb480e5
Davius <66792063+Xdavius@users.noreply.github.com>
2023-11-30 01:23:28 +0100

    Update discord.sh

--- data/discord.sh
+++ data/discord.sh
@@ -13,7 +13,10 @@ echo "Installation de Discord
 "; sleep 2
 
 echo "Installation de la clé du dépôt"
-gpg --keyring=/usr/share/keyrings/javinator9889-ppa-keyring.gpg --keyserver keyserver.ubuntu.com --recv-keys A2A43BD5139A4173 > /var/log/$LOGNAME.auto-update.txt 2>&1
+
+gpg --no-default-keyring --keyring trustedkeys.gpg --fingerprint
+
+gpg --no-default-keyring --keyring=/usr/share/keyrings/javinator9889-ppa-keyring.gpg --keyserver keyserver.ubuntu.com --recv-keys A2A43BD5139A4173 > /var/log/$LOGNAME.auto-update.txt 2>&1
 
 echo "Ajout du dépot"
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/javinator9889-ppa-keyring.gpg] https://ppa.javinator9889.com all main" | tee /etc/apt/sources.list.d/javinator9889-ppa.list >> /var/log/$LOGNAME.auto-update.txt 2>&1
